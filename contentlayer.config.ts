bd1ddee4b2081237412dc0dbacf77c5972007839
João Lucas Gomes Rocha <joao.rocha@doalti.corp>
2023-07-12 12:03:08 -0300

    refactor(contentlayer.config): adds new props in the file

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { defineDocumentType, makeSource } from "contentlayer/source-files"
+
+import readingTime from "reading-time"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypePrettyCode from "rehype-pretty-code"
+import rehypeSlug from "rehype-slug"
+import remarkGfm from "remark-gfm"
 
 const Post = defineDocumentType(() => ({
   name: "Post",
@@ -10,10 +17,54 @@ const Post = defineDocumentType(() => ({
     date: { type: "date", required: true },
     tags: { type: "list", of: { type: "string" } },
     image: { type: "string", required: true }
+  },
+  computedFields: {
+    readingTime: {
+      type: "json",
+      resolve: (doc) => readingTime(doc.body.raw)
+    },
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx?$/, "")
+    }
   }
 }))
 
 export default makeSource({
   contentDirPath: "./posts",
-  documentTypes: [Post]
+  documentTypes: [Post],
+  onExtraFieldData: "ignore",
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: "one-dark-pro",
+          onVisitLine(node: any) {
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }]
+            }
+          },
+          onVisitHighlightedLine(node: any) {
+            node.properties.className.push("line--highlighted")
+          },
+          onVisitHighlightedWord(node: any) {
+            node.properties.className = ["word--highlighted"]
+          }
+        }
+      ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section"
+          }
+        }
+      ]
+    ]
+  }
 })
