ff67d396be74702b7a29c1e21a2811ee22e71b88
João Lucas Gomes Rocha <joao.rocha@doalti.corp>
2023-07-18 14:34:30 -0300

    feat(profile): creates informations of Profile

---
+++ src/configs/profile.ts
@@ -0,0 +1,14 @@
+import { ProfileType } from "@/types"
+
+export const profileInfos: ProfileType = {
+  title: "Olá, meu nome é João Lucas!",
+  about: `Sou um desenvolvedor front-end com experiência em criação de interfaces
+  responsivas, garantindo que os aplicativos que desenvolvo sejam
+  visualmente atraentes e funcionem perfeitamente em diferentes
+  dispositivos e tamanhos de tela. Além disso, tenho experiência no
+  desenvolvimento de aplicativos móveis, criando soluções nativas e
+  eficientes. Sou um profissional comprometido, comunicativo e dedicado, o
+  que me permite colaborar de forma efetiva com a equipe.\nAtualmente estou cursando o 3º período de Análise e Desenvolvimento de
+  Sistemas na faculdade Uniube. Estou sempre buscando aprender algo novo todos os dias e me
+  aprimorando constantemente para fazer as melhores entregas.`
+}
