0d3f886f94bfa55a6970e46567c8207dc3a17dd1
João Lucas Gomes Rocha <joao.rocha@doalti.corp>
2023-07-19 22:08:41 -0300

    feat(Experiencies): creates component for renders my experiencies

---
+++ src/components/Experiencies/index.ts
@@ -0,0 +1,1 @@
+export { Experiencies } from "./Experiencies"
---
+++ src/configs/experiencies.ts
@@ -0,0 +1,25 @@
+import { ExperienciesType } from "@/types"
+
+export const experiencies: ExperienciesType = [
+  {
+    company: "Doal Soluções Empresariais",
+    href: "https://www.doalti.com.br/",
+    logoCompany: "/assets/images/doal.jpg",
+    positionCompany: "Desenvolvedor Front-End",
+    timeInCompany: "dez de 2022 - Atualmente"
+  },
+  {
+    company: "Doal Soluções Empresariais",
+    href: "https://www.doalti.com.br/",
+    logoCompany: "/assets/images/doal.jpg",
+    positionCompany: "Estágio Desenvolvimento Front-End",
+    timeInCompany: "ago de 2022 - dez de 2022"
+  },
+  {
+    company: "Qa.Coders Academy",
+    href: "https://www.qacoders.com.br/",
+    logoCompany: "/assets/images/qaCoders.jpg",
+    positionCompany: "Desenvolvedor Web Front-End",
+    timeInCompany: "mai de 2023 - Atualmente"
+  }
+]
