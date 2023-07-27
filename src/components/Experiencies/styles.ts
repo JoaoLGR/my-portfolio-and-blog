d7f512ecbd1b0e1e0e2f16297642b74779bbe1db
João Lucas Gomes Rocha <joao.rocha@doalti.corp>
2023-07-27 11:17:57 -0300

    refactor(Experiencies): envolves the render of the experiencies in the Grid component

--- src/components/Experiencies/styles.ts
+++ src/components/Experiencies/styles.ts
@@ -1,1 +1,35 @@
-//import tw from "tailwind-styled-components"
+import tw from "tailwind-styled-components"
+
+export const Container = tw.div``
+
+export const Title = tw.h2`
+  mb-6
+  text-start
+  text-base
+  font-bold
+  
+  lg:text-2xl
+`
+
+export const Card = tw.div`
+  flex
+  items-center
+  gap-4
+`
+
+export const CardContent = tw.div``
+
+export const CompanyName = tw.p`
+  text-lg
+  font-bold
+`
+
+export const TimeDescription = tw.p`
+  text-sm
+text-slate-500
+`
+
+export const PositionCompany = tw.p`
+  text-sm 
+  font-bold
+`
